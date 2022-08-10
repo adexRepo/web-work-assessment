912c8c369171e229a46e11a7b994bad03c08862e
Adek Kristiyanto <aeitiao@gmail.com>
2022-08-11 00:29:05 +0700

    register finish

--- app/App/View.php
+++ app/App/View.php
@@ -8,14 +8,14 @@ class View
 {
     public static function render(string $view, array $model = [], string $isLogin): void
     {
-        if($isLogin){
+        // if($isLogin){
 
-            require __DIR__ . '/../View/header.php';
-            require __DIR__ . '/../View/' . $view . '.php';
-            require __DIR__ . '/../View/footer.php';
+        //     require __DIR__ . '/../View/header.php';
+        //     require __DIR__ . '/../View/' . $view . '.php';
+        //     require __DIR__ . '/../View/footer.php';
 
-            return;
-        }
-        require __DIR__ . '/../View/' . $view . '/.php';
+        //     return;
+        // }
+        require __DIR__ . '/../View/' . $view . '.php';
     }
 }
--- test/Service/UserServiceTest.php
+++ test/Service/UserServiceTest.php
@@ -5,17 +5,21 @@ use PHPUnit\Framework\TestCase;
 use web\work\assessment\Config\Database;
 use web\work\assessment\Repository\UserBaseRepository;
 use web\work\assessment\Model\UserRegisterRequest;
+use web\work\assessment\Exception\ValidationException;
+use web\work\assessment\Domain\UserBase;
+
 class UserServiceTest extends TestCase
 {
     private UserService $userService;
+    private UserBaseRepository $userBaseRepository;
 
     protected function setUp():void
     {
         $connection = Database::getConnection();
-        $userBaseRepository = new UserBaseRepository($connection);
-        $this->UserService = new UserService($userBaseRepository);
-        
-        $userBaseRepository->deleteAll();
+        $this->userBaseRepository = new UserBaseRepository($connection);
+        $this->userService = new UserService( $this->userBaseRepository);
+
+        $this->userBaseRepository->deleteAll();
     }
 
     public function testRegisterSuccess() 
@@ -36,40 +40,41 @@ class UserServiceTest extends TestCase
         self::assertTrue(password_verify($req->getPassword(), $res->user->getPassword())); // cara 2
     }
 
-    // public function testRegisterFailed()
-    // {
-    //     $req = new UserRegisterRequest();
-    //     $req->     setUserId          ('user02'      );
-    //     $req->     setName            ('Uciha Itachi');
-    //     $req->     setGender          ("@"           ); // this will try to make error
-    //     $req->     setPassword        ('rahasiaAdek2');
+    public function testRegisterFailed()
+    {
+        $this->expectException(ValidationException::class);
 
-    //     $res = $this->userService->register($req);
+        $req = new UserRegisterRequest();
+        $req->setUserId('');
+        $req->setName('');
+        $req->setGender(6);
+        $req->setPassword('');
+        $this->userService->register($req);
+
+    }
+
+    public function testUserRegisterDuplicate()
+    {
+        $user = new UserBase();
+        $user->setUserId('user01');
+        $user->setName('Adek Kristiyanto');
+        $user->setGender(1);
+        $user->setPassword('rahasiaAdek1');
+
+        $this->userBaseRepository->save($user);
+
+        $this->expectException(ValidationException::class);
+
+        $req = new UserRegisterRequest();
+        $req->setUserId('user01');
+        $req->setName('Adek Kristiyanto');
+        $req->setGender(1);
+        $req->setPassword('rahasiaAdek1');
+
+        $this->userService->register($req);
+
         
-    //     self::assertEquals   ( $req->getUserId() , $res->user->getUserId  ());
-    //     self::assertEquals   ($req->getName()    , $res->user->getName    ());
-    //     self::assertEquals   (1                  , $res->user->getGender  ()); // this also try to make error
-    //     self::assertNotEquals($req->getPassword(), $res->user->getPassword()); // cara 1
 
-    //     self::assertTrue(password_verify($req->getPassword(), $res->user->getPassword())); // cara 2
-    // }
-
-    // public function testUserRegisterDuplicate()
-    // {
-    //     $req = new UserRegisterRequest ();
-    //     $req->     setUserId           ('user01'          );
-    //     $req->     setName             ('Adek Kristiyanto');
-    //     $req->     setGender           (1                 );
-    //     $req->     setPassword         ('rahasiaAdek3'    );
-
-    //     $res = $this->userService->register($req);
-        
-    //     self::assertEquals   ( $req->getUserId  (), $res->user->getUserId  ());
-    //     self::assertEquals   ( $req->getName    (), $res->user->getName    ());
-    //     self::assertEquals   ( $req->getGender  (), $res->user->getGender  ());
-    //     self::assertNotEquals( $req->getPassword(), $res->user->getPassword()); // cara 1
-
-    //     self::assertTrue(password_verify($req->getPassword(), $res->user->getPassword())); // cara 2
-    // }
+    }
 
 }
